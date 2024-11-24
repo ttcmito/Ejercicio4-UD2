ab653ca98c4201e0d8bfa06ae4fd80d4a490db0c
Mito <jaimitom1410@gmail.com>
2024-11-24 23:51:35 +0100

    area

---
+++ proyecto1/src/area/Area.java
@@ -0,0 +1,25 @@
+package area;
+
+import java.util.Scanner;
+
+public class Area {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Introduce el radio del círculo: ");
+        double radio = scanner.nextDouble();
+
+        
+        double area = Math.PI * Math.pow(radio, 2);
+
+      
+        System.out.println("El área del círculo con radio " + radio + " es: " + area);
+
+        
+        scanner.close();
+
+	}
+
+}
